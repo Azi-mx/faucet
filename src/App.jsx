0c78c3302935ece6bf8dee7702231c2dc324a828
azim308 <azim.gfuturetech@gmail.com>
2025-01-20 17:24:15 +0530

    Contract added and migrated to truffle

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="text-2xl">Hello World</div>;
+  return <div className="text-3xl">Hello World</div>;
 };
 
 export default App;
